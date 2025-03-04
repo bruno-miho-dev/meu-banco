06ccb020286ed865e7ba93cbb2316fe698379c1c
bruno-miho-dev <bruno.miho@hotmail.com>
2025-03-04 00:46:35 -0300

    Documento Routes criado e alocado as rotas do site

--- src/App.js
+++ src/App.js
@@ -1,59 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Carteira from './pages/Carteira';
-import Transferencias from './pages/Transferencias';
-import Servicos from './pages/Servicos';
-import Configuracoes from './pages/Configuracoes';
-import ProtectedRoute from './components/ProtectedRoute';
+import AppRoutes from './routes/routes';
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/carteira"
-        element={
-          <ProtectedRoute>
-            <Carteira />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/transferencias"
-        element={
-          <ProtectedRoute>
-            <Transferencias />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/servicos"
-        element={
-          <ProtectedRoute>
-            <Servicos />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/configuracoes"
-        element={
-          <ProtectedRoute>
-            <Configuracoes />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
-  </Router>
-);
+const App = () => <AppRoutes />;
 
 export default App;
