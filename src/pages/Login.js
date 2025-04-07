14b60faa1f3d87799f860a477c1d76036aec82b9
Bruno Miho <bruno.miho@hotmail.com>
2025-04-07 12:10:19 -0300

    Melhorando evento onChange dos inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -71,7 +71,7 @@ const Login = () => {
                 id="username"
                 type="text"
                 value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={({ target }) => setUsername(target.value)}
                 className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
@@ -84,7 +84,7 @@ const Login = () => {
                 id="password"
                 type="password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={({ target }) => setPassword(target.value)}
                 className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
